8543bf0257ad1af8565ce2ec7f1d880327a35f84
Marina Zhilcova <zhilcovamm@mail.ru>
2018-10-13 11:54:39 +0300

    change

--- main.cpp
+++ main.cpp
@@ -7,8 +7,8 @@ int main()
 {
     std::cout<<"build "<<version()<<" version"<<std::endl;	
     /**/
-    auto logger = spdlog::stdout_logger_mt("console");
-    logger->info("version {} was started", version());
+    //auto logger = spdlog::stdout_logger_mt("console");
+    //logger->info("version {} was started", version());
 	
     std::cout<<"Hello, world!"<<std::endl;
     return 0;
